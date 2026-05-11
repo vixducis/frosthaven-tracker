4b7b9b6ff23b1d894441319021b0302b24e80439
Wouter Henderickx <wouter.henderickx@gmail.com>
2026-05-11 21:14:33 +0200

    Fix test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -9,10 +9,19 @@ class ExampleTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_returns_a_successful_response()
+    public function test_redirects_to_campaigns_when_authenticated()
+    {
+        $user = \App\Models\User::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('home'));
+
+        $response->assertRedirect(route('campaigns.index'));
+    }
+
+    public function test_redirects_to_login_when_unauthenticated()
     {
         $response = $this->get(route('home'));
 
-        $response->assertOk();
+        $response->assertRedirect(route('login'));
     }
 }
